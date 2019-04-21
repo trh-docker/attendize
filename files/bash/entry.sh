ac82c58b87dbad986f2422d9f66612e8157056f9
SpivEgin <spivegin@txtsme.com>
2019-04-21 11:26:10 -0400

    changed entry to fit tricll

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -3,7 +3,10 @@
 # Apache gets grumpy about PID files pre-existing
 # rm -f /usr/local/apache2/logs/httpd.pid
 # /usr/sbin/apache2 &
-
+if [ -f /var/www/html/env.dist ]; then
+    cp /var/www/html/env.dist /var/www/html/.env
+    chown www-data:www-data /var/www/html/.env
+fi
 # Caddy
 caddy -conf /opt/caddy/Caddyfile &
 # Laverl key
