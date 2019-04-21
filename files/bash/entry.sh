60bbbc62251915298d373f5273435d19f635f925
SpivEgin <spivegin@txtsme.com>
2019-04-21 05:23:51 -0400

    oku

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -7,10 +7,6 @@
 # Caddy
 caddy -conf /opt/caddy/Caddyfile &
 # Laverl key
-# if [ $APP_KEY == "" ] 
-# then
-#     php artisan key:generate
-# fi
 php artisan key:generate
 # Run PHP in background
 /usr/sbin/php-fpm7.3 --nodaemonize --fpm-config /etc/php/7.3/fpm/php-fpm.conf 
